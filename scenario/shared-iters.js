85c53d597ff57dc06fa986360ab3588304bcccf4
Gabriel Nascimento <gabriel.nascimento@construo.ch>
2023-01-11 21:52:22 +0000

    executors

---
+++ scenario/shared-iters.js
@@ -0,0 +1,28 @@
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  discardResponseBodies: true,
+  scenarios: {
+    contacts: {
+      executor: 'shared-iterations',
+      vus: 10,
+      iterations: 200,
+      maxDuration: '30s',
+    },
+  },
+};
+
+export default function () {
+  http.get('https://test.k6.io/contacts.php');
+  // We're injecting a processing pause for illustrative purposes only!
+  // Each iteration will be ~515ms, therefore ~2 iterations/second per VU maximum throughput.
+  sleep(0.5);
+}
+
+export function handleSummary(data) {
+    return {
+      "summary.html": htmlReport(data),
+    };
+  }
